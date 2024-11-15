d434430e486c89e9d4b8805575cc5c806339fbb7
jagroop07 <jagroop.1408@gmail.com>
2024-11-15 19:31:43 +0530

    sV2.2

--- app/(website)/about/page.jsx
+++ app/(website)/about/page.jsx
@@ -14,21 +14,20 @@ function page() {
                 <BreadCrumbs />
             </div>
             <div className='lg:flex gap-x-20 sm:my-16 my-6'>
-                <div className='lg:w-1/2 lg:pe-4'>
-                    <h1 className='sm:text-5xl text-3xl sm:leading-[60px] text-[#000063]'>Elegant Furniture Brand in Canada</h1>
-                    <ul className='text-gray-600 text-sm sm:text-base'>
-                        <li className='my-5'>At Mainland Furniture, we believe that your home is a reflection of your personal style, a sanctuary where comfort meets elegance. With a passion for craftsmanship and a commitment to quality, we curate collections that blend timeless design with modern sophistication.</li>
-                        <li className='my-5'>Our journey began with a simple idea: to transform spaces with furniture that not only enhances aesthetics but also elevates everyday living. Each piece in our collection is thoughtfully selected to bring beauty, comfort, and functionality into your home.</li>
-                        <li className='my-5'>We take pride in offering more than just furniture; we offer a lifestyle. Whether you’re furnishing a new home or reinventing your current space, Mainland Furniture is dedicated to helping you create a living environment that is as unique as you are.</li>
-                        <li className='mt-5 lg:mb-0 mb-3'>Discover the art of fine living with Mainland Furniture, where every piece tells a story of elegance and style.</li>
-                    </ul>
-                </div>
-                <div className='lg:w-1/2 overflow-hidden'>
+                <div className='lg:w-1/2 sm:rounded-3xl overflow-hidden lg:order-2 order-1'>
                     <Image src='/images/about1.avif' className='sm:rounded-3xl sm:hover:scale-[1.2] object-cover pb-0' alt='...' height={1200} width={1200} />
+                </div>
+                <div className='lg:w-1/2 lg:pe-4 lg:order-1 lg:mt-0 mt-5 order-2'>
+                    <h1 className='sm:text-5xl text-3xl sm:leading-[60px] text-[#000063]'>Bringing Style and Comfort to Your Home</h1>
+                    <ul className='text-gray-600 text-sm sm:text-base'>
+                        <li className='my-5'>At Mainland Furniture, we are passionate about crafting elegant, timeless furniture that transforms houses into homes. Based in Canada, we believe your living space should reflect your personal style while offering comfort and functionality.</li>
+                        <li className='my-5'>Every piece in our collection is thoughtfully designed to blend sophistication with everyday practicality, helping you create a space that feels uniquely yours. Whether you're furnishing a new home or refreshing your current one, Mainland Furniture is here to inspire and elevate your living experience</li>
+                        <li className='my-5'>Discover the art of fine living with Mainland Furniture—where style meets quality.</li>
+                    </ul>
                 </div>
             </div>
             <div className='sm:my-16 my-6 lg:flex gap-20'>
-                <div className='content-center lg:w-1/2'>
+                <div className='content-center lg:w-1/2 lg:order-2 order-1'>
                     <h1 className='sm:text-5xl text-3xl text-[#000063]'>Mainland Furniture</h1>
                     <p className='text-gray-600 sm:my-4 mt-2 mb-4 sm:text-base text-sm'>Based in Canada, providing elegant furniture products.</p>
                     <div>
@@ -43,9 +42,12 @@ function page() {
                         </div>
                     </div>
                 </div>
-                <div className='bg-slate-400 lg:w-1/2 lg:mt-0 mt-3'>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d41836.954205030706!2d-122.321221!3d49.052243!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5484354495b9281f%3A0x1302a0ef2b793a93!2s2754%20Garden%20St%20Unit%202%2C%20Abbotsford%2C%20BC%20V2T%203R5%2C%20Canada!5e0!3m2!1sen!2sus!4v1731672313647!5m2!1sen!2sus" width="100%" height="400" style={{border: 0}} loading="lazy"></iframe>
+                <div className='bg-slate-400 lg:w-1/2 lg:mt-0 mt-3 lg:order-1 order-2'>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d41836.954205030706!2d-122.321221!3d49.052243!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5484354495b9281f%3A0x1302a0ef2b793a93!2s2754%20Garden%20St%20Unit%202%2C%20Abbotsford%2C%20BC%20V2T%203R5%2C%20Canada!5e0!3m2!1sen!2sus!4v1731672313647!5m2!1sen!2sus" width="100%" height="400" style={{ border: 0 }} loading="lazy"></iframe>
                 </div>
+            </div>
+            <div>
+                
             </div>
         </>
     )
